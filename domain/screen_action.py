cc198fbf3bc3c18931469c29610b402e4de6d0b6
dyzhang <997604787@qq.com>
2024-11-19 12:01:42 +0800

    fix: enhance ScreenAction

--- domain/screen_action.py
+++ domain/screen_action.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel
-from typing import Literal
+from typing import Literal, Optional
 
 
 class ScreenAction(BaseModel):
     action: Literal["click", "double_click", "hover", "select", "drag"]
     start_x: int
     start_y: int
-    end_x: int
-    end_y: int
+    end_x: Optional[int] = None
+    end_y: Optional[int] = None
