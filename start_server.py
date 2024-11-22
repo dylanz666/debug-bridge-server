3a6b9569cb2c666aae97d1494ba349f381a46219
dyzhang <997604787@qq.com>
2024-11-22 13:21:41 +0800

    fix: enhance logger

--- start_server.py
+++ start_server.py
@@ -1,24 +1,22 @@
 import subprocess
 import asyncio
-from datetime import datetime
 
 from utils.file_util import FileUtil
 
 
 async def start_uvicorn_server():
     FileUtil.makedirs_if_not_exist("log")
-    log_file = f'log/{datetime.now().strftime("%m-%d-%Y")}.log'
+    log_file = 'log/app.log'
     FileUtil.create_file_if_not_exist(log_file)
-    with open(log_file, "a") as log_file:
-        log_file.write(f"{datetime.now().strftime('%m-%d-%Y %H:%M:%S')} - Starting server...\n")
-        # start uvicorn server without any new command window opened
-        process = subprocess.Popen(
-            ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8001", "--log-config", "log-config.yaml"],
-            stdout=log_file,
-            stderr=log_file,
-            creationflags=subprocess.CREATE_NO_WINDOW
-        )
-        print("Your server is started, process id: ", process.pid)
+
+    # start uvicorn server without any new command window opened
+    process = subprocess.Popen(
+        ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8001", "--log-config", "log-config.yaml"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+        creationflags=subprocess.CREATE_NO_WINDOW
+    )
+    print("Your server is started, process id: ", process.pid)
 
 
 async def main():
