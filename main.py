48c783a68533c6654d358c7be89159670fefeddb
dyzhang <997604787@qq.com>
2024-12-03 15:27:50 +0800

    fix: enhance get_adb_devices to exclude some sn

--- main.py
+++ main.py
@@ -288,6 +288,8 @@ async def get_adb_devices():
         device_sn_list = []
 
         for line in devices_output:
+            if ":" in line:
+                continue
             if line.strip():
                 device_info = line.split()
                 device_sn = device_info[0]
