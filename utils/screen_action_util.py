b18aedde1853e91eca4c381753ac062321ced007
dyzhang <997604787@qq.com>
2024-12-05 16:38:12 +0800

    fix: except pyautogui.FailSafeException

--- utils/screen_action_util.py
+++ utils/screen_action_util.py
@@ -8,7 +8,7 @@ from domain.screen_action import ScreenAction
 
 class ScreenActionUtil:
     def __init__(self) -> None:
-        pyautogui.FAILSAFE = False
+        pass
 
     @staticmethod
     def click(screen_action: ScreenAction):
@@ -18,8 +18,11 @@ class ScreenActionUtil:
         if device_id is not None and device_id is not "":
             os.system(f"adb -s {device_id} shell input tap {start_x} {start_y}")
             return
-        pyautogui.moveTo(start_x, start_y)
-        pyautogui.click()
+        try:
+            pyautogui.moveTo(start_x, start_y)
+            pyautogui.click()
+        except pyautogui.FailSafeException:
+            pass
 
     @staticmethod
     def double_click(screen_action: ScreenAction):
@@ -31,8 +34,11 @@ class ScreenActionUtil:
             time.sleep(0.1)
             os.system(f"adb -s {device_id} shell input tap {start_x} {start_y}")
             return
-        pyautogui.moveTo(start_x, start_y)
-        pyautogui.doubleClick()
+        try:
+            pyautogui.moveTo(start_x, start_y)
+            pyautogui.doubleClick()
+        except pyautogui.FailSafeException:
+            pass
 
     @staticmethod
     def hover(screen_action: ScreenAction):
@@ -42,8 +48,11 @@ class ScreenActionUtil:
         if device_id is not None and device_id is not "":
             os.system(f"adb -s {device_id} shell input touchscreen swipe {start_x} {start_y} {start_x} {start_y} 0")
             return
-        pyautogui.moveTo(x, y, duration=0.5)
-        time.sleep(2)
+        try:
+            pyautogui.moveTo(x, y, duration=0.5)
+            time.sleep(2)
+        except pyautogui.FailSafeException:
+            pass
 
     @staticmethod
     def select(screen_action: ScreenAction):
@@ -56,14 +65,17 @@ class ScreenActionUtil:
             os.system(f"adb -s {device_id} shell input touchscreen swipe {start_x} {start_y} {start_x} {start_y} 0")
             os.system(f"adb -s {device_id} shell input touchscreen swipe {start_x} {start_y} {end_x} {end_y} 100")
             return
-        # move to start position
-        pyautogui.moveTo(start_x, start_y, duration=0.25)
-        # hold the left key of mouse
-        pyautogui.mouseDown()
-        # move to end position to select
-        pyautogui.moveTo(end_x, end_y, duration=0.25)
-        # release mouse
-        pyautogui.mouseUp()
+        try:
+            # move to start position
+            pyautogui.moveTo(start_x, start_y, duration=0.25)
+            # hold the left key of mouse
+            pyautogui.mouseDown()
+            # move to end position to select
+            pyautogui.moveTo(end_x, end_y, duration=0.25)
+            # release mouse
+            pyautogui.mouseUp()
+        except pyautogui.FailSafeException:
+            pass
 
     @staticmethod
     def drag(screen_action: ScreenAction):
@@ -75,16 +87,22 @@ class ScreenActionUtil:
         if device_id is not None and device_id is not "":
             os.system(f"adb -s {device_id} shell input touchscreen swipe {start_x} {start_y} {end_x} {end_y} 500")
             return
-        pyautogui.moveTo(start_x, start_y, duration=0.5)
-        pyautogui.dragTo(end_x, end_y, duration=1)
+        try:
+            pyautogui.moveTo(start_x, start_y, duration=0.5)
+            pyautogui.dragTo(end_x, end_y, duration=1)
+        except pyautogui.FailSafeException:
+            pass
 
     @staticmethod
     def mouse_right(screen_action):
         device_id = screen_action.device_id
         if device_id is not None and device_id is not "":
             return
-        pyautogui.moveTo(screen_action.start_x, screen_action.start_y, duration=0.5)
-        pyautogui.click(button='right')
+        try:
+            pyautogui.moveTo(screen_action.start_x, screen_action.start_y, duration=0.5)
+            pyautogui.click(button='right')
+        except pyautogui.FailSafeException:
+            pass
 
     @staticmethod
     def keyboard_input(screen_action):
@@ -101,18 +119,21 @@ class ScreenActionUtil:
         control_keys = ["enter", "delete", "backspace", "escape", "capslock", "shift", "alt", "up", "down", "left",
                         "right", "tab"]
         lower_main_key = main_key.lower()
-        if lower_main_key in control_keys and (bind_key is None or bind_key == ""):
-            main_key = main_key.lower()
-            pyautogui.press(main_key)
-            return
-        if lower_main_key == "control" and (bind_key is None or bind_key == ""):
-            main_key = "ctrl"
-            pyautogui.press(main_key)
-            return
-        if lower_main_key == "control" and bind_key is not None and bind_key != "":
-            pyautogui.hotkey('ctrl', bind_key)
-            return
-        if lower_main_key in ["shift", "alt"] and bind_key is not None and bind_key != "":
-            pyautogui.hotkey(lower_main_key, bind_key)
-            return
-        pyautogui.typewrite(main_key)
+        try:
+            if lower_main_key in control_keys and (bind_key is None or bind_key == ""):
+                main_key = main_key.lower()
+                pyautogui.press(main_key)
+                return
+            if lower_main_key == "control" and (bind_key is None or bind_key == ""):
+                main_key = "ctrl"
+                pyautogui.press(main_key)
+                return
+            if lower_main_key == "control" and bind_key is not None and bind_key != "":
+                pyautogui.hotkey('ctrl', bind_key)
+                return
+            if lower_main_key in ["shift", "alt"] and bind_key is not None and bind_key != "":
+                pyautogui.hotkey(lower_main_key, bind_key)
+                return
+            pyautogui.typewrite(main_key)
+        except pyautogui.FailSafeException:
+            pass
